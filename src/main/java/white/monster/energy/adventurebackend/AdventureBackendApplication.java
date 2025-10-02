d75835b3d62c1b392a56fe1386b87b2e52e2c9e0
Simon <46815527+SimonGotfred@users.noreply.github.com>
2025-10-02 11:45:39 +0200

    initial

---
+++ src/main/java/white/monster/energy/adventurebackend/AdventureBackendApplication.java
@@ -0,0 +1,15 @@
+package white.monster.energy.adventurebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AdventureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/white/monster/energy/adventurebackend/AdventureBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package white.monster.energy.adventurebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
