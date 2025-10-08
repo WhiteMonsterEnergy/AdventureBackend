43f4c086964a071ea99402a04316c3ffb10af2c4
Simon <sigo0001@stud.ek.dk>
2025-10-08 11:57:40 +0200

    ABS-43: configured POM with Mockito, and created classes for testing

--- src/test/java/white/monster/energy/adventurebackend/AdventureBackendApplicationTests.java
+++ src/test/java/white/monster/energy/adventurebackend/AdventureBackendApplicationTests.java
@@ -6,10 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AdventureBackendApplicationTests
 {
-
+    /*
     @Test
     void contextLoads()
     {
     }
-
+    */
 }
---
+++ src/test/java/white/monster/energy/adventurebackend/BookingTests.java
@@ -0,0 +1,9 @@
+package white.monster.energy.adventurebackend;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BookingTests
+{
+}
