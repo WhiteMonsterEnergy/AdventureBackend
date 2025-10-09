64ad1849194f944ef6bc10ee4125a3af3b5de912
AndLOLGG <andreassilas@gmail.com>
2025-10-09 10:55:17 +0200

    Added comment to Lucia's fix to BookingAccessRepository explaining the Query and the alias 'a'

--- src/main/java/white/monster/energy/adventurebackend/profile/BookingAccessRepository.java
+++ src/main/java/white/monster/energy/adventurebackend/profile/BookingAccessRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BookingAccessRepository extends JpaRepository<BookingAccess, Integer> {
+    // Custom query to find all bookingIds by profileId, where a is the alias for BookingAccess entity
     @Query("SELECT a.bookingId FROM BookingAccess a WHERE a.profileId = :profileId")
 List<Integer> findBookingIdsByProfileId(Integer profileId);
 void deleteByProfileId(Integer profileId);
