ce4c8d2c9eeae90277af7ff9db5ec1ebeea3b854
Lucia <asqv0001@stud.ek.dk>
2025-10-09 10:36:36 +0200

    added @Query annotation in BookingAccessRepository.java

--- src/main/java/white/monster/energy/adventurebackend/profile/BookingAccessRepository.java
+++ src/main/java/white/monster/energy/adventurebackend/profile/BookingAccessRepository.java
@@ -1,10 +1,12 @@
 package white.monster.energy.adventurebackend.profile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookingAccessRepository extends JpaRepository<BookingAccess, Integer> {
+@Query
 List<Integer> findBookingIdsByProfileId(Integer profileId);
 void deleteByProfileId(Integer profileId);
 
